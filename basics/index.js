5308833b5ee51e32c832fec492155f4fd33442c4
SAMHITHA288 <samhithabattula841@gmail.com>
2025-02-07 14:05:48 +0530

    created fruits end point

--- basics/index.js
+++ basics/index.js
@@ -1,3 +1,18 @@
 const express = require ("express")
 const app =express()
-const port =
+const port = 4000
+app.get ("/",(req,res)=>{
+    res.send("server started successfully")
+})
+app.get("/fruits",(req,res)=>{
+    res.json({
+        endpoint : " Fruits ",
+        apples: "🍎🍏",
+        mangos:"🥭🥭🥭🥭",
+        melon:"🍉🍉"
+    })
+}
+)
+app.listen(port,()=>{
+    console.log(`\n\t server started`)
+})
